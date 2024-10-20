a4722659d3c7808f0112f64915a77d39b3bff097
Asdmir786 <asmir.alams.com@gmail.com>
2024-10-20 12:48:35 +0500

    p3 Done

--- Python Tutorial For Beginners in Hindi/Chapter 9/p3.py
+++ Python Tutorial For Beginners in Hindi/Chapter 9/p3.py
@@ -1,2 +1,12 @@
 print("Here are the tables of 2 to 20 in seperate files.")
 
+def generateTable(n):
+  table = ""
+  for i in range(1, 11):
+    table += f"{n} X {i} = {n*i}\n"
+
+  with open(f"tables/table_{n}.txt", "w") as f:
+    f.write(table)
+
+for i in range(2, 21):
+  generateTable(i)
