eb5127a330437b8015bc492802131291c0eceefe
Asdmir786 <asmir.alams.com@gmail.com>
2024-10-25 20:05:47 +0500

    p3 done

---
+++ Python Tutorial For Beginners in Hindi/Chapter 12/p3.py
@@ -0,0 +1,9 @@
+number = int(input("Gimme a Number and I will give you a list of it from 1 to 10\n: "))
+
+listah = []
+
+for i in range(1,11):
+    tempvar = number*i
+    listah.append(tempvar)
+
+print(listah)
